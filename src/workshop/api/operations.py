7b4f86127131f299fb6cb40c096898eab6976ea3
simonovmaksim <simonov.maksim13@yandex.ru>
2022-07-11 17:32:29 +0300

    Finished CRUD for Operations

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,9 +1,8 @@
-from typing import List
+from typing import List, Optional
 
-import tables
-from database import get_session
-from fastapi import APIRouter, Depends
-from models.operations import Operation
+from fastapi import APIRouter, Depends, Response, status
+from models.operations import Operation, OperationCreate, OperationKind, OperationUpdate
+from services.operations import OperationService
 from sqlalchemy.orm import Session
 
 router = APIRouter(
@@ -12,10 +11,45 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[Operation])
-def get_operations(session: Session = Depends(get_session)):
-    operations = (
-        session
-        .query(tables.Operation)
-        .all()
+def get_operations(
+    kind: Optional[OperationKind] = None,
+    service: OperationService = Depends()
+):
+    return service.get_list(kind=kind)
+
+
+@router.post('/', response_model=Operation)
+def create_operation(
+    operation_data: OperationCreate,
+    service: OperationService = Depends(),
+):
+    return service.create(operation_data)
+
+
+@router.get('/{operation_id}', response_model=Operation)
+def get_operation(
+    operation_id: int,
+    service: OperationService = Depends(),
+):
+    return service.get(operation_id)
+
+@router.put('/{opration_id}', response_model=Operation)
+def update_operation(
+    operation_id: int,
+    operation_data: OperationUpdate,
+    service: OperationService = Depends(),
+):
+    return service.update(
+        operation_id,
+        operation_data,
     )
-    return operations
+
+
+
+@router.delete('/{opration_id}')
+def delete_operation(
+    operation_id: int,
+    service: OperationService = Depends(),
+):
+    service.delete(operation_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/workshop/models/operations.py
+++ src/workshop/models/operations.py
@@ -6,16 +6,26 @@ from datetime import date
 
 
 class OperationKind(str, Enum):
-    INCOME = 'income',
+    INCOME = 'income'
     OUTCOME = 'outcome'
 
-
-class Operation(BaseModel):
-    id: int
+class OperationBase(BaseModel):
     date: date
     kind: OperationKind
     amount: Decimal
     description: Optional[str]
 
+
+class Operation(OperationBase):
+    id: int
+
     class Config:
         orm_mode = True
+
+
+class OperationCreate(OperationBase):
+    pass
+
+
+class OperationUpdate(OperationBase):
+    pass
